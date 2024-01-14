54144c86dcc5d2fb6c828d700315b33f07cac7b1
Enzo Sastrokarijo <enzo@sastrokarijo.nl>
2024-01-14 04:47:13 +0100

    feat: add pack selection screen

--- lib/src/features/game_selection/view/game_selection.dart
+++ lib/src/features/game_selection/view/game_selection.dart
@@ -1,22 +1,138 @@
+import 'package:bottoms_up/src/common_widgets/header_text.dart';
 import 'package:bottoms_up/src/common_widgets/home_appbar.dart';
-import 'package:bottoms_up/src/common_widgets/info_square.dart';
-import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:bottoms_up/src/core/exports.dart';
+import 'package:bottoms_up/src/features/game_selection/view/game_selection_controller.dart';
+import 'package:bottoms_up/src/router/app_routing.dart';
 import 'package:bottoms_up/src/theming/thema_data.dart' as theme;
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class GameSelectionScreen extends ConsumerWidget {
+class GameSelectionScreen extends StatelessWidget {
   const GameSelectionScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return ColoredScaffold(
-        body: InfoSquare(
-      body: 'Here will be the game selection screen',
-      leading: const Icon(
-        Icons.info,
-        size: 40,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const HomeAppBar(),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const HeaderText('SELECT GAME'),
+                  const Divider(
+                    color: Colors.white,
+                  ),
+                  GameTypeCard(
+                    color: theme.blue,
+                    gamePack: GamePack.breakingTheIce,
+                  ),
+                  GameTypeCard(
+                    color: theme.green,
+                    gamePack: GamePack.raisingTheStakes,
+                  ),
+                  GameTypeCard(
+                    color: theme.orange,
+                    gamePack: GamePack.caliente,
+                  ),
+                  const Divider(
+                    color: Colors.white,
+                  ),
+                  HomeButtonFilled(
+                    onTap: () {
+                      context.goNamed(AppRoute.gameScreen.name);
+                    },
+                    color: theme.yellow,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const FaIcon(
+                            FontAwesomeIcons.play,
+                            size: 40,
+                            color: Colors.black,
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'START GAME'.hardcoded,
+                            style: GoogleFonts.lexendGiga(
+                              textStyle: const TextStyle(
+                                  fontSize: 48,
+                                  color: Colors.black,
+                                  height: 1,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
-      color: theme.orange,
-    ));
+    );
+  }
+}
+
+class GameTypeCard extends ConsumerWidget {
+  final Color color;
+
+  final GamePack gamePack;
+  const GameTypeCard({
+    super.key,
+    required this.color,
+    required this.gamePack,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedPack = ref.watch(selectedPackProvider);
+
+    return Padding(
+      padding: const EdgeInsets.all(4),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text(
+              'Breaking The Ice',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w800,
+                fontSize: 25,
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Checkbox(
+              fillColor: MaterialStateProperty.all(Colors.white),
+              value: selectedPack[gamePack.name],
+              onChanged: (_) {
+                ref.read(selectedPackProvider.notifier).changeGame(gamePack);
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/src/features/game_selection/view/game_selection_controller.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'game_selection_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$selectedPackHash() => r'36d0d0dfc0a1f53341097eb311efef2bb020b6a7';
+
+/// See also [SelectedPack].
+@ProviderFor(SelectedPack)
+final selectedPackProvider =
+    AutoDisposeNotifierProvider<SelectedPack, Map<String, bool>>.internal(
+  SelectedPack.new,
+  name: r'selectedPackProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$selectedPackHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SelectedPack = AutoDisposeNotifier<Map<String, bool>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
