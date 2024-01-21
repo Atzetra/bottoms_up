f2d3ce8a6dfd1b0fe6af52824e9a21d16c92d643
Enzo Sastrokarijo <enzo@sastrokarijo.nl>
2024-01-21 02:13:01 +0100

    feat: switch over to PocketBase

--- lib/src/features/game/data/card_service.dart
+++ lib/src/features/game/data/card_service.dart
@@ -1,27 +1,22 @@
-import 'package:appwrite/appwrite.dart';
 import 'package:bottoms_up/src/core/env/env.dart';
+import 'package:bottoms_up/src/features/game/domain/gamecard_data.dart';
 import 'package:bottoms_up/src/features/game_selection/view/game_selection_controller.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:pocketbase/pocketbase.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'card_service.g.dart';
 
 class CardService {
-  final Databases database = Databases(Client()
-      .setEndpoint(Env.appwriteEndpoint)
-      .setProject(Env.appwriteProject));
+  final pb = PocketBase(Env.pocketbaseEndpoint);
 
-  Future<List<dynamic>> getCards(GamePack pack) async {
-    try {
-      final documents = await database.listDocuments(
-          collectionId: Env.collectionId,
-          databaseId: Env.databaseId,
-          queries: [Query.equal('pack', pack.name)]);
+  Future<List<GameCardData>> getCards(GamePack pack) async {
+    final documents = await pb.collection('cards').getFullList(
+          filter: 'gamePack = "${pack.name}"',
+        );
 
-      return documents.documents;
-    } on AppwriteException catch (e) {
-      debugPrint(e.message);
-      throw Exception(e.message);
-    }
+    debugPrint(documents.toString());
+
+    return documents.map((e) => GameCardData.fromJson(e.data)).toList();
   }
 }
 
